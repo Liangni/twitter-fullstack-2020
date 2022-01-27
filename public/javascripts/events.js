207832150f3ac6fadcd258fad0479982fa85bdb6
Liangni <light0104@gmail.com>
2022-01-27 13:03:11 +0800

    send request to GET/ api/users/:userId  and insert data into editUserModal

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -32,60 +32,79 @@ addButton.addEventListener('click', function check(event) {
 })
 
 
-const coverInput = document.querySelector('#cover-input')
-const coverWrapper = document.querySelector('#cover-wrapper')
-const currentCover = document.querySelector('#current-cover')
-const deleteCoverBtn = document.querySelector('#delete-cover-btn')
-const avatarInput = document.querySelector('#avatar-input')
-const currentAvatar = document.querySelector('#current-avatar')
-const avatarWrapper = document.querySelector('#avatar-wrapper')
+const editUserModal = document.querySelector('#editUserModal')
 
+editUserModal.addEventListener('change', function onFileChanged(event) {
+  const fileInput = event.target
 
-coverInput.addEventListener('change', function showCoverPreview(event) {
-
-  if (event.target.files.length !== 0) {
-    const src = URL.createObjectURL(event.target.files[0])
-    let previewArea = document.createElement('div')
-    previewArea.innerHTML = `
-      <img src="${src}" class="cover" id="file-cover-preview">
-    `
-    // 若是初次選擇檔案，要先隱藏使用者封面，並加上刪除按鈕
-    if (!currentCover.classList.contains('hide')) {
-      currentCover.classList.add('hide')
-      deleteCoverBtn.classList.remove('hide')
+  if (fileInput.files.length !== 0) {
+    if (fileInput.matches('#cover-input')) {
+      showCoverPreview(fileInput)
     }
-    //　若是重複選擇檔案，先把前一個選擇的圖片移除
-    if (coverWrapper.children.length > 1) {
-      coverWrapper.lastElementChild.remove()
+    if (fileInput.matches('#avatar-input')) {
+      showAvatarPreview(fileInput)
     }
-    coverWrapper.appendChild(previewArea)
   }
 })
 
+editUserModal.addEventListener('click',
+  function onModalClicked(event) {
+    if (event.target.matches('#delete-cover-btn')) {
+      deleteCoverPreview()
+    }
+  })
 
-deleteCoverBtn.addEventListener('click', function deleteCoverPreview() {
+function showCoverPreview(coverInput) {
+  const coverWrapper = document.querySelector('#cover-wrapper')
+  const currentCover = document.querySelector('#current-cover')
+  const deleteCoverBtn = document.querySelector('#delete-cover-btn')
+  const lastPreview = document.querySelector('#file-cover-preview') || null // 如抓不到節點表示未選取過檔案，帶入null值
+  const newPreview = document.createElement('div')
+  const src = URL.createObjectURL(coverInput.files[0])
+  
+  // 若是初次選擇檔案，要先隱藏使用者封面，並加上刪除按鈕
+  if (!currentCover.classList.contains('hide')) {
+    currentCover.classList.add('hide')
+    deleteCoverBtn.classList.remove('hide')
+  }
+  //　若是重複選擇檔案，先把前一個選擇的圖片移除
+  if (lastPreview) {
+    lastPreview.remove()
+  }
+  
+  newPreview.innerHTML = `
+      <img src="${src}" class="cover" id="file-cover-preview">
+    `
+  coverWrapper.appendChild(newPreview)
+}
+
+function showAvatarPreview(avatarInput) {
+  const avatarWrapper = document.querySelector('#avatar-wrapper')
+  const currentAvatar = document.querySelector('#current-avatar')
+  const lastPreview = document.querySelector('#file-avatar-preview') || null // 如抓不到節點表示未選取過檔案，帶入null值
+  const newPreview = document.createElement('div')
+  const src = URL.createObjectURL(avatarInput.files[0])
+  
+  // 若是初次選擇檔案，要先隱藏使用者頭貼
+  if (!currentAvatar.classList.contains('hide')) {
+    currentAvatar.classList.add('hide')
+  }
+  //　若是重複選擇檔案，要先把前一個選擇的圖片移除
+  if (lastPreview) { lastPreview.remove() }
+
+  newPreview.innerHTML = `
+      <img src="${src}" class="avatar large" id="file-avatar-preview">
+    `
+  avatarWrapper.appendChild(newPreview)
+}
+
+function deleteCoverPreview() {
+  const coverWrapper = document.querySelector('#cover-wrapper')
+  const currentCover = document.querySelector('#current-cover')
+  const deleteCoverBtn = document.querySelector('#delete-cover-btn')
+
   coverInput.value = ""
   coverWrapper.lastElementChild.remove()
   currentCover.classList.remove('hide')
   deleteCoverBtn.classList.add('hide')
-})
-
-avatarInput.addEventListener('change', function showAvatarPreview(event) {
-
-  if (event.target.files.length !== 0) {
-    const src = URL.createObjectURL(event.target.files[0])
-    let previewArea = document.createElement('div')
-    previewArea.innerHTML = `
-      <img src="${src}" class="avatar large" id="file-avatar-preview">
-    `
-    // 若是初次選擇檔案，要先隱藏使用者頭貼
-    if (!currentAvatar.classList.contains('hide')) {
-      currentAvatar.classList.add('hide')
-    }
-    //　若是重複選擇檔案，要先把前一個選擇的圖片移除
-    if (avatarWrapper.children.length > 1) {
-      avatarWrapper.lastElementChild.remove()
-    }
-    avatarWrapper.appendChild(previewArea)
-  }
-})
+}
---
+++ public/javascripts/userProfile.js
@@ -0,0 +1,63 @@
+const userProfileControlPanel = document.querySelector('#user-profile-control-panel')
+
+userProfileControlPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('#edit-user-button')) {
+    const coverInput = document.querySelector('#cover-input')
+    const deleteCoverBtn = document.querySelector('#delete-cover-btn')
+    const avatarInput = document.querySelector('#avatar-input')
+
+    // 如曾開啟edit-user-modal並選擇cover檔案
+    if (coverInput.files.length !== 0) {
+      // 清空coverInput的檔案
+      coverInput.value = ''
+      // 隱藏移除預覽按鈕
+      deleteCoverBtn.classList.add('hide')
+    }
+
+    // 如曾開啟edit-user-modal並選擇avatar檔案
+    if (avatarInput.files.length !== 0) {
+      // 清空avatarInput的檔案
+      avatarInput.value = ''
+    }
+
+    renderEditUserModal(event.target.dataset.userId)
+  }
+})
+
+function renderEditUserModal(userId) {
+  axios
+  .get(`/api/users/${userId}`)
+  .then((response) => {
+    const user = response.data
+    const coverWrapper = document.querySelector('#cover-wrapper')
+    const avatarWrapper = document.querySelector('#avatar-wrapper')
+    const formNameColumn = document.querySelector('#form-name-column')
+    const formIntroColumn = document.querySelector('#form-intro-column')
+
+    if (user.cover) {
+      coverWrapper.innerHTML = `
+        <img class="cover" id="current-cover" src="${ user.cover }" alt="">
+      `
+    } 
+    if (user.avatar) {
+      avatarWrapper.innerHTML =`
+      <img class="avatar large" id="current-avatar" src="${ user.avatar }"></img>
+      `
+    }
+    if (user.introduction) {
+      formIntroColumn.innerHTML = `
+    <label for="introduction">自我介紹</label>
+    <textarea class="form-control" id="introduction" name="introduction"
+    rows="3">${ user.introduction }</textarea>
+    `
+    }
+    formNameColumn.innerHTML = `
+      <label for="name">名稱</label>
+      <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" value="${user.name}" required>
+    `
+  })
+  .catch(function (error) {
+      console.log(error)
+  })
+}
+
