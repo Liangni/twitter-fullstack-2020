80ad6aaaa84c618aad60d5c85c2724a96a38bdde
Liangni <light0104@gmail.com>
2022-03-04 17:26:39 +0800

    Modify file-helpers

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,13 +1,12 @@
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+imgur.setClientID(IMGUR_CLIENT_ID)
 
 function getImgurLink(file) {
   return new Promise((resolve, reject) => {
-    if (!file) {
-      return resolve(null)
-    }
-    imgur.setClientID(IMGUR_CLIENT_ID)
-    imgur.upload(file[0].path, (err, img) => {
+    if (!file) return resolve(null)
+    
+    return imgur.upload(file[0].path, (err, img) => {
       if (err) {
         return reject(err)
       }
