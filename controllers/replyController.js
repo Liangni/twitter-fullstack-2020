fd3c53784ac9defe284cdc49ffbecc19bff845a1
Liangni <light0104@gmail.com>
2022-03-08 12:34:25 +0800

    Refactor postReply and add err handler

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -5,18 +5,24 @@ const helpers = require('../_helpers')
 
 const replyController = {
   //設定新增留言
-  postReply: (req, res) => {
-    return Reply.create({
-      comment: req.body.comment,
-      TweetId: req.params.tweetId,
-      UserId: helpers.getUser(req).id
-    }).then(reply => {
-      return Tweet.findOne({ where: { id: reply.TweetId } }).then(tweet => {
-        return tweet.increment('replyCounts')
-      }).then(tweet => {
-        return res.redirect(`/tweets/${req.params.tweetId}/replies`)
+  postReply: (req, res, next) => {
+    const tweetId = req.params.tweetId
+
+    return Tweet.findByPk(tweetId)
+      .then(tweet => {
+        if (!tweet) throw new Error('回應的貼文不存在!')
+
+        return Promise.all([
+          Reply.create({
+            comment: req.body.comment,
+            TweetId: tweetId,
+            UserId: helpers.getUser(req).id
+          }),
+          tweet.increment('replyCounts')
+        ])
       })
-    })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
   }
 }
 
