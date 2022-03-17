3b536d1b6ba0340aca6cd7d50c8cd74ce534c35f
Liangni <light0104@gmail.com>
2022-03-17 12:28:31 +0800

    Add route variable to navbar partial to identify current path

--- public/javascripts/navbar.js
+++
@@ -1,8 +0,0 @@
-const currentPath = window.location.pathname
-const isTweetsPath = /^\/tweets/.test(currentPath)
-const isUsersPath = /^\/users/.test(currentPath)
-const navLinks = document.querySelectorAll('.nav-link')
-
-if (isTweetsPath) { navLinks[0].classList.add('active') }
-if (isUsersPath && !currentPath.includes('setting') ) { navLinks[1].classList.add('active') }
-if (isUsersPath && currentPath.includes('setting')) { navLinks[2].classList.add('active') }
